c69caac3afe355e50d971d6a99490f84cd2559ea
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-08-07 16:45:50 -0400

    Fixed formatting

--- twilio-power-outlet.rb
+++ twilio-power-outlet.rb
@@ -3,27 +3,27 @@ require 'twilio-ruby'
 require 'pusher'
 
 configure do
-	Pusher.app_id = ENV['PUSHER_APP_ID']
-	Pusher.key = ENV['PUSHER_KEY']
-	Pusher.secret = ENV['PUSHER_SECRET']
+  Pusher.app_id = ENV['PUSHER_APP_ID']
+  Pusher.key = ENV['PUSHER_KEY']
+  Pusher.secret = ENV['PUSHER_SECRET']
 end
 
 post '/control/?' do
-	output = "Message transmitted"
-	begin
-		Pusher['robot_channel'].trigger('powersms', {:command => params['Body'].downcase})
+  output = "Message transmitted"
+  begin
+    Pusher['robot_channel'].trigger('powersms', {:command => params['Body'].downcase})
   rescue Pusher::Error => e
     output = "Failed: #{e.message}"
   end
 
-	if params['web'] == '1'
-		erb :index, :locals => {:msg => output}
-	else
-		response = Twilio::TwiML::Response.new do |r|
-	  	r.Sms output
-		end
-		response.text
-	end
+  if params['web'] == '1'
+    erb :index, :locals => {:msg => output}
+  else
+    response = Twilio::TwiML::Response.new do |r|
+      r.Sms output
+    end
+    response.text
+  end
 end
 
 get '/' do
