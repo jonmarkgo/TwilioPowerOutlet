a1930988d330c545808bc89b2583dad6856b5d9b
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-08-07 12:10:21 -0400

    fix for heroku

--- config.ru
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'bundler'
-
-Bundler.require
-
-require './twilio-power-outlet'
-
-run TwilioPowerOutlet
--- twilio-power-outlet.rb
+++ twilio-power-outlet.rb
@@ -1,15 +1,16 @@
-class TwilioPowerOutlet < Sinatra::Base
-  configure do
-    Pusher.app_id = ENV['PUSHER_APP_ID']
-    Pusher.key = ENV['PUSHER_KEY']
-    Pusher.secret = ENV['PUSHER_SECRET']
-  end
+require 'sinatra'
+require 'twilio-ruby'
 
-  get_or_post '/sms/?' do
-  	Pusher['robot_channel'].trigger('powersms', {:Body => params['Body'].downcase, :From => params['From']})
-    response = Twilio::TwiML::Response.new do |r|
-      r.Sms 'Received'
-    end
-    response.text
-  end
+configure do
+	Pusher.app_id = ENV['PUSHER_APP_ID']
+	Pusher.key = ENV['PUSHER_KEY']
+	Pusher.secret = ENV['PUSHER_SECRET']
+end
+
+get_or_post '/sms/?' do
+	Pusher['robot_channel'].trigger('powersms', {:Body => params['Body'].downcase, :From => params['From']})
+	response = Twilio::TwiML::Response.new do |r|
+  	r.Sms 'Received'
+	end
+	response.text
 end
